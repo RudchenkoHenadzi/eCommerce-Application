c481ed0d31b01e15d2e886cae938c234f16e92cd
Henadzi Rudchenko <grudchenko93@yandex.by>
2024-05-05 09:06:48 +0300

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import TheHeader from '@/components/TheHeader.vue'
 </script>
 
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-      <RouterLink to="/authorization">Authorization</RouterLink>
-      <RouterLink to="/catalog">Catalog</RouterLink>
-      <RouterLink to="/product">Product</RouterLink>
-      <RouterLink to="/cart-shopping">Cart shopping</RouterLink>
-      <RouterLink to="/user">User</RouterLink>
-    </nav>
-  </header>
-
+  <TheHeader />
   <main>
     <RouterView />
   </main>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,65 @@
+<script lang="ts">
+import { RouterLink } from 'vue-router'
+
+export default {
+  name: 'TheHeader'
+}
+</script>
+<template>
+  <header class="header">
+    <nav class="navigation">
+      <ul class="navigation__list">
+        <li class="navigation__item">
+          <RouterLink class="navigation__link" to="/">Home</RouterLink>
+        </li>
+        <li class="navigation__item">
+          <RouterLink class="navigation__link" to="/about">About</RouterLink>
+        </li>
+        <li class="navigation__item">
+          <RouterLink class="navigation__link" to="/authorization">Authorization</RouterLink>
+        </li>
+        <li class="navigation__item">
+          <RouterLink class="navigation__link" to="/catalog">Catalog</RouterLink>
+        </li>
+        <li class="navigation__item">
+          <RouterLink class="navigation__link" to="/product">Product</RouterLink>
+        </li>
+        <li class="navigation__item">
+          <RouterLink class="navigation__link" to="/cart-shopping">Cart shopping</RouterLink>
+        </li>
+        <li class="navigation__item">
+          <RouterLink class="navigation__link" to="/user">User</RouterLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/variables.scss';
+@import '@/assets/styles/extends.scss';
+@import '@/assets/styles/fonts.scss';
+
+.navigation {
+  background: $color-bachground;
+  &__list {
+    display: flex;
+    gap: 10px;
+    padding: 10px 0;
+    @extend %clear-ul;
+    @extend %container-1100;
+  }
+
+  &__item {
+  }
+
+  &__link {
+    font-size: 14px;
+    color: $color-black;
+    @extend %clear-a;
+    &:hover {
+      color: $color-purple;
+    }
+  }
+}
+</style>
