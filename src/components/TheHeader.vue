e073d83e29977c45e08a19f9b8fe6c531bc6cf54
Henadzi Rudchenko <grudchenko93@yandex.by>
2024-05-05 10:48:17 +0300

    refactor: formatting code

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,71 +1,17 @@
-<script lang="ts">
-import { RouterLink } from 'vue-router'
-
-export default {
-  name: 'TheHeader'
-}
-</script>
 <template>
   <header class="header">
-    <nav class="navigation">
-      <ul class="navigation__list">
-        <li class="navigation__item">
-          <RouterLink class="navigation__link" to="/">Home</RouterLink>
-        </li>
-        <li class="navigation__item">
-          <RouterLink class="navigation__link" to="/about">About</RouterLink>
-        </li>
-        <li class="navigation__item">
-          <RouterLink class="navigation__link" to="/authorization">Authorization</RouterLink>
-        </li>
-        <li class="navigation__item">
-          <RouterLink class="navigation__link" to="/catalog">Catalog</RouterLink>
-        </li>
-        <li class="navigation__item">
-          <RouterLink class="navigation__link" to="/product">Product</RouterLink>
-        </li>
-        <li class="navigation__item">
-          <RouterLink class="navigation__link" to="/cart-shopping">Cart shopping</RouterLink>
-        </li>
-        <li class="navigation__item">
-          <RouterLink class="navigation__link" to="/user">User</RouterLink>
-        </li>
-      </ul>
-    </nav>
+    <TheNavigation />
   </header>
 </template>
 
-<style lang="scss" scoped>
-@import '@/assets/styles/variables.scss';
-@import '@/assets/styles/extends.scss';
-@import '@/assets/styles/fonts.scss';
+<script lang="ts">
+import { RouterLink } from 'vue-router'
+import TheNavigation from '@/components/TheNavigation.vue'
 
-.navigation {
-  font-weight: 500;
-  background: $color-bachground;
-  &__list {
-    display: flex;
-    gap: 10px;
-    padding-top: 10px;
-    padding-bottom: 10px;
-    @extend %clear-ul;
-    @extend %container-1100;
-  }
-
-  &__item {
-  }
-
-  &__link {
-    font-size: 14px;
-    color: $color-black;
-    @extend %clear-a;
-    &:hover {
-      color: $color-purple;
-    }
+export default {
+  name: 'TheHeader',
+  components: {
+    TheNavigation
   }
 }
-
-.router-link-active {
-  color: $color-purple;
-}
-</style>
+</script>
---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,63 @@
+<template>
+  <nav class="navigation">
+    <ul class="navigation__list">
+      <li class="navigation__item">
+        <RouterLink class="navigation__link" to="/">Home</RouterLink>
+      </li>
+      <li class="navigation__item">
+        <RouterLink class="navigation__link" to="/about">About</RouterLink>
+      </li>
+      <li class="navigation__item">
+        <RouterLink class="navigation__link" to="/authorization">Authorization</RouterLink>
+      </li>
+      <li class="navigation__item">
+        <RouterLink class="navigation__link" to="/catalog">Catalog</RouterLink>
+      </li>
+      <li class="navigation__item">
+        <RouterLink class="navigation__link" to="/product">Product</RouterLink>
+      </li>
+      <li class="navigation__item">
+        <RouterLink class="navigation__link" to="/cart-shopping">Cart shopping</RouterLink>
+      </li>
+      <li class="navigation__item">
+        <RouterLink class="navigation__link" to="/user">User</RouterLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/variables.scss';
+@import '@/assets/styles/extends.scss';
+@import '@/assets/styles/fonts.scss';
+
+.navigation {
+  font-weight: 500;
+  background: $color-bachground;
+  &__list {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    @extend %clear-ul;
+    @extend %container-1100;
+  }
+
+  &__item {
+  }
+
+  &__link {
+    font-size: 14px;
+    color: $color-black;
+    @extend %clear-a;
+    &:hover {
+      color: $color-purple;
+    }
+  }
+}
+
+.router-link-active {
+  color: $color-purple;
+}
+</style>
