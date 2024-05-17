30a2de7d3c43f5a3021416bc0ca9ecd2d0210673
Nastya Pisanka <bonjozzy@gmail.com>
2024-05-17 16:25:08 +0300

    chore: add SDK and isomorfic fetch
    (to fix an error 'global is undefined')

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,13 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), VueDevTools()],
+  define: {
+    global: {},
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'node-fetch': 'isomorphic-fetch',
     }
   }
 })
