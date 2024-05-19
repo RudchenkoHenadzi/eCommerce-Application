9fd1e911af12ca34235ee83a52e950b5857be9cb
Henadzi Rudchenko <grudchenko93@yandex.by>
2024-05-19 19:21:06 +0300

    refactor: formatting code

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,85 +1,27 @@
 <template>
-  <form class="login-form" @submit.prevent="submitLoginForm()">
-    <div class="login-form__title">Вход</div>
-    <div class="login-form__wrapper">
-      <div class="login-form__item">
-        <div class="login-form__label">Email:</div>
-        <input
-          type="email"
-          id="login-email"
-          v-model.trim="loginForm.email"
-          placeholder="Введите email"
-          :class="{
-            invalid:
-              (v$.loginForm.email.$dirty && !v$.loginForm.email.required.$response) ||
-              (v$.loginForm.email.$dirty && !v$.loginForm.email.email.$response)
-          }"
-          class="login-form__input input"
-          name="email"
-        />
-
-        <div v-for="error of v$.loginForm.email.$errors" :key="error.$uid" class="invalid-message">
-          {{ error.$message }}
-        </div>
-      </div>
-      <div class="login-form__item">
-        <div class="login-form__label">Пароль:</div>
-        <div class="login-form__input-wrapper">
-          <input
-            :type="inputType"
-            id="login-password"
-            v-model.trim="loginForm.password"
-            name="password"
-            placeholder="Введите пароль"
-            class="login-form__input input"
-            :class="{
-              invalid:
-                (v$.loginForm.password.$dirty && !v$.loginForm.password.required.$response) ||
-                (v$.loginForm.password.$dirty && !v$.loginForm.password.minLength.$response) ||
-                (v$.loginForm.password.$dirty && v$.loginForm.password.regexPassword.$response)
-            }"
-          />
-          <EyeIconSVG
-            class="login-form__password-icon"
-            @click="inputType = 'text'"
-            v-if="inputType === 'password'"
-          />
-          <EyeCrossedIconSVG
-            class="login-form__password-icon"
-            @click="inputType = 'password'"
-            v-else
-          />
-        </div>
-
-        <div
-          v-for="error of v$.loginForm.password.$errors"
-          :key="error.$uid"
-          class="invalid-message"
-        >
-          {{ error.$message }}
-        </div>
-      </div>
+  <form class="form login-form" @submit.prevent="submitLoginForm()">
+    <div class="form__title">Вход</div>
+    <div class="form__wrapper">
+      <InputEmail v-model="loginForm.email" />
+      <InputPassword v-model="loginForm.password" />
     </div>
-    <button type="submit" class="login-form__btn button-white">Войти</button>
-    <RouterLink class="login-form__switch" to="/registration">Зарегистрироваться</RouterLink>
+    <button type="submit" class="form__btn button-white">Войти</button>
+    <RouterLink class="form__switch" to="/registration">Зарегистрироваться</RouterLink>
   </form>
 </template>
 
 <script lang="ts">
 import axios from 'axios'
 import useValidate from '@vuelidate/core'
-import { required, email, helpers, minLength } from '@vuelidate/validators'
-import EyeIconSVG from '@/Icons/EyeIconSVG.vue'
-import EyeCrossedIconSVG from '@/Icons/EyeCrossedIconSVG.vue'
-
-const regexPassword = helpers.regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/)
+import InputEmail from '@/components/form-elements/InputEmail.vue'
+import InputPassword from '@/components/form-elements/InputPassword.vue'
 
 export default {
   name: 'LoginForm',
 
   components: {
-    EyeIconSVG,
-    EyeCrossedIconSVG
+    InputEmail,
+    InputPassword
   },
 
   setup() {
@@ -93,16 +35,13 @@ export default {
       loginForm: {
         email: '',
         password: ''
-      },
-      inputType: 'password'
+      }
     }
   },
 
-  mounted() {},
-
   methods: {
     async submitLoginForm() {
-      const result = await this.v$.loginForm.$validate()
+      const result = await this.v$.$validate()
 
       if (result) {
         axios
@@ -118,23 +57,6 @@ export default {
         console.log('В форме есть ошибки')
       }
     }
-  },
-
-  validations: {
-    loginForm: {
-      email: {
-        required: helpers.withMessage('Введите email', required),
-        email: helpers.withMessage('Не корректный email', email)
-      },
-      password: {
-        required: helpers.withMessage('Введите пароль', required),
-        minLength: helpers.withMessage(
-          `Пароль должен быть больше ${minLength(8).$params.min} символов`,
-          minLength(8)
-        ),
-        regexPassword: helpers.withMessage('Слабый пароль', regexPassword)
-      }
-    }
   }
 }
 </script>
@@ -145,65 +67,6 @@ export default {
 @import '@/assets/styles/extends.scss';
 
 .login-form {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: stretch;
-  gap: 20px;
-  width: 100%;
-  max-width: 480px;
-  padding: 30px 20px;
-  background-color: $color-purple;
-  color: $color-white;
-  border-radius: 10px;
-
-  @media (width <=480px) {
-    border-radius: 0;
-  }
-
-  &__wrapper {
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
-    gap: 20px;
-    margin: 0;
-    padding: 0;
-    border: none;
-  }
-
-  &__title {
-    font-size: 20px;
-    text-align: center;
-    text-transform: uppercase;
-  }
-
-  &__item {
-    display: flex;
-    flex-direction: column;
-    gap: 5px;
-    width: 100%;
-  }
-
-  &__input-wrapper {
-    position: relative;
-  }
-
-  &__password-icon {
-    position: absolute;
-    top: 50%;
-    right: 10px;
-    transform: translateY(-50%);
-    width: 30px;
-    fill: $color-white;
-    cursor: pointer;
-  }
-
-  &__switch {
-    height: auto;
-    text-align: center;
-    text-decoration: none;
-    color: $color-white;
-  }
+  margin-top: 50px;
 }
 </style>
--- src/components/RegistrationForm.vue
+++ src/components/RegistrationForm.vue
@@ -1,266 +1,56 @@
 <template>
-  <form class="registration-form" @submit.prevent="submitRegistrationForm()">
-    <div class="registration-form__title">Регистрация</div>
-    <div class="registration-form__wrapper">
-      <div class="registration-form__row">
-        <div class="registration-form__item">
-          <div class="registration-form__label">Имя:</div>
-          <input
-            type="text"
-            id="registration-name"
-            v-model.trim="registrationForm.name"
-            name="name"
-            placeholder="Введите имя"
-            class="registration-form__input input"
-            :class="{
-              invalid:
-                (v$.registrationForm.name.$dirty && !v$.registrationForm.name.required.$response) ||
-                (v$.registrationForm.name.$dirty &&
-                  v$.registrationForm.name.regexSpecialSymbol.$response)
-            }"
-          />
-          <div
-            v-for="error of v$.registrationForm.name.$errors"
-            :key="error.$uid"
-            class="invalid-message"
-          >
-            {{ error.$message }}
-          </div>
-        </div>
-        <div class="registration-form__item">
-          <div class="registration-form__label">Фамилия:</div>
-          <input
-            type="text"
-            id="registration-lastname"
-            v-model.trim="registrationForm.lastname"
-            name="lastname"
-            placeholder="Введите фамилию"
-            class="registration-form__input input"
-            :class="{
-              invalid:
-                (v$.registrationForm.lastname.$dirty &&
-                  !v$.registrationForm.lastname.required.$response) ||
-                (v$.registrationForm.name.$dirty &&
-                  v$.registrationForm.name.regexSpecialSymbol.$response)
-            }"
-          />
-          <div
-            v-for="error of v$.registrationForm.lastname.$errors"
-            :key="error.$uid"
-            class="invalid-message"
-          >
-            {{ error.$message }}
-          </div>
-        </div>
+  <form id="registration-form" class="form" @submit.prevent="submitRegistrationForm()">
+    <div class="form__title">Регистрация</div>
+    <div class="form__wrapper">
+      <div class="form__row">
+        <InputName v-model="registrationForm.name" />
+        <InputLastName v-model="registrationForm.lastname" />
       </div>
-      <div class="registration-form__item">
-        <div class="registration-form__label">Дата рождения:</div>
-        <input
-          type="date"
-          id="registration-date"
-          v-model="registrationForm.date"
-          name="date"
-          class="registration-form__input input"
-          :class="{
-            invalid:
-              (v$.registrationForm.date.$dirty && !v$.registrationForm.date.required.$response) ||
-              (v$.registrationForm.date.$dirty && !v$.registrationForm.date.minValue)
-          }"
-        />
-        <div
-          v-for="error of v$.registrationForm.date.$errors"
-          :key="error.$uid"
-          class="invalid-message"
-        >
-          {{ error.$message }}
-        </div>
+      <InputDate v-model="registrationForm.date" />
+      <div class="form__row">
+        <InputEmail v-model="registrationForm.email" />
+        <InputPassword v-model="registrationForm.password" />
       </div>
-      <div class="registration-form__row">
-        <div class="registration-form__item">
-          <div class="registration-form__label">Email:</div>
-          <input
-            type="email"
-            id="login-email"
-            v-model.trim="registrationForm.email"
-            name="email"
-            placeholder="Введите email"
-            :class="{
-              invalid:
-                (v$.registrationForm.email.$dirty &&
-                  !v$.registrationForm.email.required.$response) ||
-                (v$.registrationForm.email.$dirty && !v$.registrationForm.email.email.$response)
-            }"
-            class="registration-form__input input"
-          />
-
-          <div
-            v-for="error of v$.registrationForm.email.$errors"
-            :key="error.$uid"
-            class="invalid-message"
-          >
-            {{ error.$message }}
-          </div>
-        </div>
-        <div class="registration-form__item">
-          <div class="registration-form__label">Пароль:</div>
-          <div class="registration-form__input-wrapper">
-            <input
-              :type="inputType"
-              id="login-password"
-              v-model.trim="registrationForm.password"
-              name="password"
-              placeholder="Введите пароль"
-              class="registration-form__input input"
-              :class="{
-                invalid:
-                  (v$.registrationForm.password.$dirty &&
-                    !v$.registrationForm.password.required.$response) ||
-                  (v$.registrationForm.password.$dirty &&
-                    !v$.registrationForm.password.minLength.$response) ||
-                  (v$.registrationForm.password.$dirty &&
-                    v$.registrationForm.password.regexPassword.$response)
-              }"
-            />
-            <EyeIconSVG
-              class="registration-form__password-icon"
-              @click="inputType = 'text'"
-              v-if="inputType === 'password'"
-            />
-            <EyeCrossedIconSVG
-              class="registration-form__password-icon"
-              @click="inputType = 'password'"
-              v-else
-            />
-          </div>
-
-          <div
-            v-for="error of v$.registrationForm.password.$errors"
-            :key="error.$uid"
-            class="invalid-message"
-          >
-            {{ error.$message }}
-          </div>
-        </div>
+      <div class="form__row">
+        <InputCity v-model="registrationForm.city" />
+        <InputStreet v-model="registrationForm.street" />
       </div>
-      <div class="registration-form__row">
-        <div class="registration-form__item">
-          <div class="registration-form__label">Город:</div>
-          <input
-            type="text"
-            id="registration-city"
-            v-model.trim="registrationForm.city"
-            name="city"
-            placeholder="Ваш город"
-            class="registration-form__input input"
-            :class="{
-              invalid:
-                (v$.registrationForm.city.$dirty && !v$.registrationForm.city.required.$response) ||
-                (v$.registrationForm.city.$dirty &&
-                  !v$.registrationForm.city.regexSpecialSymbol.$response)
-            }"
-          />
-          <div
-            v-for="error of v$.registrationForm.city.$errors"
-            :key="error.$uid"
-            class="invalid-message"
-          >
-            {{ error.$message }}
-          </div>
-        </div>
-        <div class="registration-form__item">
-          <div class="registration-form__label">Улица:</div>
-          <input
-            type="text"
-            id="registration-street"
-            v-model.trim="registrationForm.street"
-            name="street"
-            placeholder="Ваша улица"
-            class="registration-form__input input"
-            :class="{
-              invalid:
-                v$.registrationForm.street.$dirty && !v$.registrationForm.street.required.$response
-            }"
-          />
-          <div
-            v-for="error of v$.registrationForm.street.$errors"
-            :key="error.$uid"
-            class="invalid-message"
-          >
-            {{ error.$message }}
-          </div>
-        </div>
-      </div>
-      <div class="registration-form__row">
-        <div class="registration-form__item">
-          <div class="registration-form__label">Почтовый индекс:</div>
-          <input
-            type="number"
-            id="registration-postcode"
-            v-model.trim="registrationForm.postcode"
-            name="postcode"
-            placeholder="Почтовый индекс"
-            class="registration-form__input input"
-            :class="{
-              invalid:
-                v$.registrationForm.postcode.$dirty &&
-                !v$.registrationForm.postcode.required.$response
-            }"
-          />
-          <div
-            v-for="error of v$.registrationForm.postcode.$errors"
-            :key="error.$uid"
-            class="invalid-message"
-          >
-            {{ error.$message }}
-          </div>
-        </div>
-        <div class="registration-form__item">
-          <div class="registration-form__label">Страна:</div>
-          <input
-            type="text"
-            id="registration-country"
-            v-model.trim="registrationForm.country"
-            name="country"
-            placeholder="Страна"
-            class="registration-form__input input"
-            :class="{
-              invalid:
-                v$.registrationForm.country.$dirty &&
-                !v$.registrationForm.country.required.$response
-            }"
-            disabled
-          />
-          <div
-            v-for="error of v$.registrationForm.country.$errors"
-            :key="error.$uid"
-            class="invalid-message"
-          >
-            {{ error.$message }}
-          </div>
-        </div>
+      <div class="form__row">
+        <InputPotscode v-model="registrationForm.postcode" />
+        <InputCountry v-model="registrationForm.country" />
       </div>
     </div>
-    <button type="submit" class="registration-form__btn button-white">Регистрация</button>
-    <RouterLink class="registration-form__switch" to="/authorization">Войти</RouterLink>
+    <button type="submit" class="form__btn button-white">Регистрация</button>
+    <RouterLink class="form__switch" to="/authorization">Войти</RouterLink>
   </form>
 </template>
 
 <script lang="ts">
 import axios from 'axios'
 import useValidate from '@vuelidate/core'
-import { required, email, helpers, minLength, maxLength, minValue } from '@vuelidate/validators'
-import EyeIconSVG from '@/Icons/EyeIconSVG.vue'
-import EyeCrossedIconSVG from '@/Icons/EyeCrossedIconSVG.vue'
-
-const regexPassword = helpers.regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/)
-const regexSpecialSymbol = helpers.regex(/^[a-zA-Zа-яА-ЯёЁ]+$/)
+import InputName from '@/components/form-elements/InputName.vue'
+import InputLastName from '@/components/form-elements/InputLastName.vue'
+import InputDate from '@/components/form-elements/InputDate.vue'
+import InputEmail from '@/components/form-elements/InputEmail.vue'
+import InputPassword from '@/components/form-elements/InputPassword.vue'
+import InputStreet from '@/components/form-elements/InputStreet.vue'
+import InputCity from '@/components/form-elements/InputCity.vue'
+import InputPotscode from '@/components/form-elements/InputPotscode.vue'
+import InputCountry from '@/components/form-elements/InputCountry.vue'
 
 export default {
   name: 'RegistrationForm',
 
   components: {
-    EyeIconSVG,
-    EyeCrossedIconSVG
+    InputName,
+    InputLastName,
+    InputDate,
+    InputEmail,
+    InputPassword,
+    InputStreet,
+    InputCity,
+    InputPotscode,
+    InputCountry
   },
 
   setup() {
@@ -281,14 +71,13 @@ export default {
         city: '',
         postcode: '',
         country: 'Россия'
-      },
-      inputType: 'password'
+      }
     }
   },
 
   methods: {
     async submitRegistrationForm() {
-      const result = await this.v$.registrationForm.$validate()
+      const result = await this.v$.$validate()
       if (result) {
         axios
           .post('https://jsonplaceholder.typicode.com/posts')
@@ -303,131 +92,12 @@ export default {
         console.log('В форме есть ошибки')
       }
     }
-  },
-
-  validations: {
-    registrationForm: {
-      name: {
-        required: helpers.withMessage('Введите имя', required),
-        regexSpecialSymbol: helpers.withMessage('Недопустимые символы', regexSpecialSymbol)
-      },
-      lastname: {
-        required: helpers.withMessage('Введите фамилию', required),
-        regexSpecialSymbol: helpers.withMessage('Недопустимые символы', regexSpecialSymbol)
-      },
-      date: {
-        required: helpers.withMessage('Укажите вашу дату рождения', required),
-        minValue: helpers.withMessage('Неверная дата', minValue('2000-01-01'))
-      },
-      email: {
-        required: helpers.withMessage('Введите email', required),
-        email: helpers.withMessage('Не корректный email', email)
-      },
-      password: {
-        required: helpers.withMessage('Введите пароль', required),
-        minLength: helpers.withMessage(
-          `Пароль должен быть больше ${minLength(8).$params.min} символов`,
-          minLength(8)
-        ),
-        regexPassword: helpers.withMessage('Слабый пароль', regexPassword)
-      },
-      street: {
-        required: helpers.withMessage('Укажите улицу', required),
-        minLength: helpers.withMessage(
-          `Улица должнабыть ${minLength(8).$params.min} символов`,
-          minLength(8)
-        )
-      },
-      city: {
-        required: helpers.withMessage('Укажите город', required),
-        regexSpecialSymbol: helpers.withMessage('Недопустимые символы', regexSpecialSymbol)
-      },
-      postcode: {
-        required: helpers.withMessage('Укажите почтовый индекс', required),
-        minLength: helpers.withMessage(`Укажите ${minLength(6).$params.min} цифр`, minLength(6)),
-        maxLength: helpers.withMessage(`Укажите ${maxLength(6).$params.max} цифр`, maxLength(6))
-      },
-      country: {
-        required: helpers.withMessage('Укажите страну', required)
-      }
-    }
   }
 }
 </script>
 
-<style lang="scss" scoped>
-@import '@/assets/styles/variables.scss';
-@import '@/assets/styles/mixins.scss';
-@import '@/assets/styles/extends.scss';
-
-.registration-form {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: stretch;
-  gap: 20px;
-  width: 100%;
-  max-width: 480px;
-  padding: 30px 20px;
-  background-color: $color-purple;
-  color: $color-white;
-  border-radius: 10px;
-
-  @media (width <=380px) {
-    border-radius: 0;
-  }
-
-  &__wrapper {
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: stretch;
-    gap: 15px;
-    margin: 0;
-    padding: 0;
-    border: none;
-  }
-
-  &__title {
-    font-size: 20px;
-    text-align: center;
-    text-transform: uppercase;
-  }
-
-  &__row {
-    display: flex;
-    gap: 10px;
-    @media (width <= 480px) {
-      flex-direction: column;
-    }
-  }
-
-  &__item {
-    display: flex;
-    flex-direction: column;
-    gap: 5px;
-    width: 100%;
-  }
-
-  &__input-wrapper {
-    position: relative;
-  }
-
-  &__password-icon {
-    position: absolute;
-    top: 50%;
-    right: 10px;
-    transform: translateY(-50%);
-    width: 30px;
-    fill: $color-white;
-    cursor: pointer;
-  }
-
-  &__switch {
-    height: auto;
-    text-align: center;
-    text-decoration: none;
-    color: $color-white;
-  }
+<style scoped lang="scss">
+#registration-form {
+  margin-top: 50px;
 }
 </style>
