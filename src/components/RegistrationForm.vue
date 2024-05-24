23b97770161f4f6f2a20e7ea1d4eeed8b5dd2dfe
Nastya Pisanka <bonjozzy@gmail.com>
2024-05-24 22:12:06 +0300

    feat: add additional field to the registration form

--- src/components/RegistrationForm.vue
+++ src/components/RegistrationForm.vue
@@ -18,6 +18,7 @@
         <MyCheckbox
           name="shipping"
           id="shipping-checkbox"
+          v-model="registrationForm.isShippingAddressDefault"
           text="Установить адресом доставки по-умолчанию"
         />
         <div class="form__row">
@@ -33,24 +34,31 @@
           <InputCountry v-model="registrationForm.shippingAddress.country" />
         </div>
       </div>
+      <MyCheckbox
+        name="sameAddresses"
+        id="same-addresses-checkbox"
+        text="Адрес доставки совпадает с платежным адресом"
+        v-model="registrationForm.areBothAddressesSame"
+      />
       <div class="form__block block billing-address">
         <p class="block__title">Платежный адрес</p>
         <MyCheckbox
           name="billing"
           id="billing-checkbox"
+          v-model="registrationForm.isBillingAddressDefault"
           text="Установить платежным адресом по-умолчанию"
         />
         <div class="form__row">
-          <InputCity v-model="registrationForm.shippingAddress.city" />
-          <InputStreet v-model="registrationForm.shippingAddress.streetName" />
+          <InputCity v-model="billingAddressCity" />
+          <InputStreet v-model="billingAddressStreetName" />
         </div>
         <div class="form__row">
-          <InputBuilding v-model="registrationForm.shippingAddress.building" />
-          <InputApartment v-model="registrationForm.shippingAddress.apartment" />
+          <InputBuilding v-model="billingAddressBuilding" />
+          <InputApartment v-model="billingAddressApartment" />
         </div>
         <div class="form__row">
-          <InputPotscode v-model="registrationForm.shippingAddress.postalCode" />
-          <InputCountry v-model="registrationForm.shippingAddress.country" />
+          <InputPotscode v-model="billingAddressPostalCode" />
+          <InputCountry v-model="billingAddressCountry" />
         </div>
       </div>
     </div>
@@ -72,9 +80,9 @@ import InputPotscode from '@/components/form-elements/InputPotscode.vue'
 import InputCountry from '@/components/form-elements/InputCountry.vue'
 import InputBuilding from '@/components/form-elements/InputBuilding.vue'
 import InputApartment from '@/components/form-elements/InputApartment.vue'
-import apiRootStore from '@/stores/ApiRootStore'
 import { createCustomerDraft } from '@/helpers/createDataSamples'
 import MyCheckbox from '@/components/form-elements/radio/MyCheckbox.vue'
+import { useApiRootStore } from '@/stores/CTClient'
 
 export default {
   name: 'RegistrationForm',
@@ -116,11 +124,21 @@ export default {
           postalCode: '',
           country: 'Россия'
           // mobile: ''
-        }
+        },
+        billingAddress: {
+          streetName: '',
+          building: '',
+          apartment: '',
+          city: '',
+          postalCode: '',
+          country: 'Россия'
+        },
+        isBillingAddressDefault: false,
+        isShippingAddressDefault: false,
+        areBothAddressesSame: false
       }
     }
   },
-
   methods: {
     async submitRegistrationForm() {
       const result = await this.v$.$validate()
@@ -159,7 +177,9 @@ export default {
           building,
           apartment
         )
-        apiRootStore.registerUser(customerDraft)
+        //apiRootStore.registerUser(customerDraft)
+        const apiRoot = useApiRootStore()
+        await apiRoot.registerUser(customerDraft)
         console.log('В форме ошибок нет, можем отправлять')
       } else {
         console.log('В форме есть ошибки')
@@ -181,6 +201,97 @@ export default {
         console.log('В форме есть ошибки')
       }
     }*/
+  },
+  computed: {
+    billingAddressStreetName: {
+      get(): string {
+        return this.registrationForm.areBothAddressesSame
+          ? this.registrationForm.shippingAddress.streetName
+          : this.registrationForm.billingAddress.streetName
+      },
+      set(newValue: string) {
+        if (this.registrationForm.areBothAddressesSame) {
+          this.registrationForm.billingAddress.streetName =
+            this.registrationForm.shippingAddress.streetName
+        } else {
+          this.registrationForm.billingAddress.streetName = newValue
+        }
+      }
+    },
+    billingAddressBuilding: {
+      get(): string {
+        return this.registrationForm.areBothAddressesSame
+          ? this.registrationForm.shippingAddress.building
+          : this.registrationForm.billingAddress.building
+      },
+      set(newValue: string) {
+        if (this.registrationForm.areBothAddressesSame) {
+          this.registrationForm.billingAddress.building =
+            this.registrationForm.shippingAddress.building
+        } else {
+          this.registrationForm.billingAddress.building = newValue
+        }
+      }
+    },
+    billingAddressApartment: {
+      get(): string {
+        return this.registrationForm.areBothAddressesSame
+          ? this.registrationForm.shippingAddress.apartment
+          : this.registrationForm.billingAddress.apartment
+      },
+      set(newValue: string) {
+        if (this.registrationForm.areBothAddressesSame) {
+          this.registrationForm.billingAddress.apartment =
+            this.registrationForm.shippingAddress.apartment
+        } else {
+          this.registrationForm.billingAddress.apartment = newValue
+        }
+      }
+    },
+    billingAddressCity: {
+      get(): string {
+        return this.registrationForm.areBothAddressesSame
+          ? this.registrationForm.shippingAddress.city
+          : this.registrationForm.billingAddress.city
+      },
+      set(newValue: string) {
+        if (this.registrationForm.areBothAddressesSame) {
+          this.registrationForm.billingAddress.city = this.registrationForm.shippingAddress.city
+        } else {
+          this.registrationForm.billingAddress.city = newValue
+        }
+      }
+    },
+    billingAddressPostalCode: {
+      get(): string {
+        return this.registrationForm.areBothAddressesSame
+          ? this.registrationForm.shippingAddress.postalCode
+          : this.registrationForm.billingAddress.postalCode
+      },
+      set(newValue: string) {
+        if (this.registrationForm.areBothAddressesSame) {
+          this.registrationForm.billingAddress.postalCode =
+            this.registrationForm.shippingAddress.postalCode
+        } else {
+          this.registrationForm.billingAddress.postalCode = newValue
+        }
+      }
+    },
+    billingAddressCountry: {
+      get(): string {
+        return this.registrationForm.areBothAddressesSame
+          ? this.registrationForm.shippingAddress.country
+          : this.registrationForm.billingAddress.country
+      },
+      set(newValue: string) {
+        if (this.registrationForm.areBothAddressesSame) {
+          this.registrationForm.billingAddress.country =
+            this.registrationForm.shippingAddress.country
+        } else {
+          this.registrationForm.billingAddress.country = newValue
+        }
+      }
+    }
   }
 }
 </script>
