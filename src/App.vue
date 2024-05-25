53565534d79484003cc4ff22867e5e40e81b283f
Nastya Pisanka <bonjozzy@gmail.com>
2024-05-25 12:22:14 +0300

    reafctor: remove apiRoot from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,17 @@
 <script lang="ts">
 import { RouterView } from 'vue-router'
 import TheHeader from '@/components/TheHeader.vue'
-import { useApiRootStore } from '@/stores/CTClient'
 
 export default {
   components: { TheHeader, RouterView },
   name: 'App',
-  mounted() {
-    const apiRoot = useApiRootStore()
-    apiRoot.start()
-  }
 }
 </script>
 
 <template>
   <TheHeader />
   <main>
-    <RouterView apiRoot="apiRoot" />
+    <RouterView />
   </main>
 </template>
 
